8783406dbc2f432e537201e0a3c6af83cefa195a
JACK-QBS <2200838299@qq.com>
2020-09-23 23:17:21 +0800

    数据类型

---
+++ 9月份/Test.java
@@ -0,0 +1,21 @@
+public class Test {
+    public static void main(String[] args){
+//        System.out.println("Hello World!");
+        /*char ch = 'A';
+        System.out.println(ch);
+        char c = '帅';
+        System.out.println(c);*/
+
+        /*byte value = 0 ;
+        System.out.println(value);*/
+
+        /*short value = 0;
+        System.out.println(value);*/
+
+        /*boolean value = true;
+        System.out.println(value);*/
+
+        String name = "观众老爷都是帅哥";
+        System.out.println(name);
+    }
+}
