3e01e21072211b41abf29085c0424fb42b25c75b
JACK-QBS <2200838299@qq.com>
2020-09-24 23:42:51 +0800

    运算符1

---
+++ 9月份/test2.java
@@ -0,0 +1,87 @@
+package test24;
+
+public class test2 {
+    /**
+     * 运算符注意事项：
+     * /     %
+     * @param args
+     */
+    public static void main1(String[] args) {
+        /*int a = 10;
+        int b = 2;
+        System.out.println(a/b);*/
+
+        /*System.out.println(5/2);
+        System.out.println(5/2.0);
+        System.out.println((float)5/2);
+        System.out.println(5/(float)2);*/
+
+        /*System.out.println(10%3);
+        System.out.println(10%-3);
+        System.out.println(-10%3);
+        System.out.println(-10%-3);*/
+
+        /*int i = 10;
+        i = i++;
+        System.out.println(i);*/
+
+        //关系运算符返回的都是Boolean类型
+        /*int a = 10;
+        int b = 2;
+        System.out.println(a == b);*/
+
+        /*int i = 10;
+        int j = ++i;
+        System.out.println(j);*/
+
+    }
+
+    /**
+     *  作业代码
+     * @param args
+     */
+    public static void main(String[] args){
+        /*short a = 128;
+        byte b = (byte) a;
+        System.out.println("a = "+ a + "b = " +b);*/
+
+        /*double x = 2.0;
+        int y = 4;
+        x /= ++y;
+        System.out.println(x);*/
+
+        //System.out.println(11.5 % 2.0);
+        /*int a = 10;
+        a += 1;// 等价于 a = a+1
+        System.out.println(a);*/
+
+        /*int a = 10;
+        int b = ++a;
+        System.out.println(b);
+        int c = a++;
+        System.out.println(c);*/
+
+        /*int a = 10;
+        int b = 20;
+        System.out.println(a == b);
+        System.out.println(a != b);*/
+
+        /*int a = 10;
+        int b = 20;
+        int c = 30;
+        System.out.println(a < b && b < c);*/
+
+        //System.out.println(10 > 20 && 20 < 30);
+      /*  int a = 10;
+        int b = 20;
+        int c = 30;
+        System.out.println(a < b || a < c);*/
+
+        int a = 10;
+        int b = 20;
+        System.out.println(! (a<b));
+
+    }
+
+
+}
