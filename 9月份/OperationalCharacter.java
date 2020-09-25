232c72277ed904acb8987a19502a14d4811b5869
JACK-QBS <2200838299@qq.com>
2020-09-25 13:01:21 +0800

    运算符 2

---
+++ 9月份/OperationalCharacter.java
@@ -0,0 +1,29 @@
+package test24;
+
+public class OperationalCharacter {// 运算符
+    public static void main (String[] args) {
+        /*int a = 10;
+        int b = 20;
+        System.out.println(a & b);*/
+
+        /*int a = 10;
+        int b = 20;
+        System.out.println(a | b);*/
+
+//        int a = 0xf;
+//        System.out.printf("%x\n", ~a);
+        /*int a = 0xa;
+        int b = 0xc;
+        System.out.printf("%x\n",a^b);*/
+
+        /*int a = 0x10;
+        System.out.printf("%x\n", a >>1);*/
+
+        int a = 10;
+        int b = 20;
+        int max = a > b ? a : b;
+        System.out.println(max);
+
+
+    }
+}
